42b4eb71efb25ce1f337790826fc24516307cfee
xiongfan <xiongfan@moonai.com.cn>
2020-12-30 17:04:20 +0800

    jsonarray格式的字符串解析

--- src/main/scala/FastJson.scala
+++ src/main/scala/FastJson.scala
@@ -2,6 +2,7 @@ import com.alibaba.fastjson.JSON
 
 object FastJson {
   def main(args: Array[String]): Unit = {
+    //解析json格式的字符串，对于jsonarray形式的字符串，可以组装成json格式的字符串再使用json.getJSONArray(key).get(0)方法（小白目前还不知道其他方法）
     val jsonString = "{\"et\":\"kanqiu_client_join\",\"vtm\":1435898329434,\"body\":{\"client\":\"866963024862254\",\"" +
       "client_type\":\"android\",\"room\":\"NBA_HOME\",\"gid\":\"\",\"type\":\"\",\"roomid\":\"\"}," +
       "\"time\":[{\"arrayKey\":\"arrayVal\"},{\"key2\":\"val2\"}]}"
